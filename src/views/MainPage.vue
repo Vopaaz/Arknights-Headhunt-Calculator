84fa8c33f91788646ec38ee080ef80a6da8c2611
Vopaaz <liyifan945@163.com>
2020-05-07 23:51:18 +0800

    Setup LineChart

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -8,6 +8,7 @@
         </b-col>
         <b-col lg="4" offset-lg="0" md="8" offset-md="2" align-self="center">
           <result-card message="成功出货概率" :probability="baseProbability" />
+          <line-chart :chart-data="chartData" :options="options"></line-chart>
         </b-col>
       </b-row>
     </b-container>
@@ -17,18 +18,51 @@
 <script>
 import BaseInput from "../components/BaseInput.vue";
 import ResultCard from "../components/ResultCard.vue";
+import LineChart from "../components/Chart.vue";
 import { calculateProbability } from "../js/calc";
-import * as _ from "lodash"
+import * as _ from "lodash";
+
+function getChartData(target, targetStar) {
+  return {
+    labels: ["0", "1", "2", "3", "4", "5", "6+"],
+    datasets: [
+      {
+        label: "目标干员",
+        backgroundColor: "#f87979",
+        data: target
+      },
+      {
+        label: "同星级干员",
+        backgroundColor: "#123456",
+        data: targetStar
+      }
+    ]
+  };
+}
 
 export default {
   data: function() {
     return {
-      baseProbability: 0
+      baseProbability: 0,
+      chartData: getChartData([1, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0]),
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                max: 1
+              }
+            }
+          ]
+        }
+      }
     };
   },
   components: {
     BaseInput,
-    ResultCard
+    ResultCard,
+    LineChart
   },
   methods: {
     onChange: function() {
@@ -44,7 +78,8 @@ export default {
         d.thisPrevDraw,
         d.limited
       );
-      this.baseProbability = _.sum(result.target) - result.target[0]
+      this.baseProbability = _.sum(result.target) - result.target[0];
+      this.chartData = getChartData(result.target, result.targetStar);
     }
   }
 };
