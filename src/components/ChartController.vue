f25679d3b7415eb9679724434daf66425be0f3fb
Vopaaz <liyifan945@163.com>
2020-05-09 00:35:37 +0800

    Fix typo in math

--- src/components/ChartController.vue
+++ src/components/ChartController.vue
@@ -17,7 +17,7 @@
     <b-alert show dismissible>
       PMF: 恰好抽到对应次数的概率
       <br />CDF: 抽到对应次数及以上的概率，即
-      <span v-katex="'\\text{CDF}(X \\geq x)'"></span>
+      <span v-katex="'\\text{Pr}(X \\geq x)'"></span>
     </b-alert>
   </div>
 </template>
